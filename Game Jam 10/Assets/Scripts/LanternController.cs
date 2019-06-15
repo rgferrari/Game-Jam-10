ad5f8b7ff73cba0a532a2748d80ebdb3c28e7d58
rgferrari <rgferrari@inf.ufsm.br>
2019-06-15 12:14:04 -0300

    Update

---
+++ Game Jam 10/Assets/Scripts/LanternController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class LanternController : MonoBehaviour
+{
+    public Text countdown; //UI Text Object
+    public float timeLeft = 1f * 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown.text = ("" + timeLeft);
+    }
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
