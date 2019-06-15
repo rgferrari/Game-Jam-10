5a846f55ee2ea5a3f369e40b5c6bef1050a2cec3
rgferrari <rgferrari@inf.ufsm.br>
2019-06-14 23:54:09 -0300

    Adicionado tilemap

---
+++ Game Jam 10/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Transform target;
+
+    private float smoothSpeed = 10f;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = new Vector3(0, 0, -10);
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Game Jam 10/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D playerRigidbody2D;
+    private float moveSpeed = 100f;
+    float horizontalMove;
+    float verticalMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal"); 
+        verticalMove = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(horizontalMove * moveSpeed * Time.fixedDeltaTime, verticalMove * moveSpeed * Time.fixedDeltaTime);
+        playerRigidbody2D.velocity = movement;
+    }
+}
