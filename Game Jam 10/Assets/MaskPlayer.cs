3d3072e77c8db4ecae72c4a35d571ad9cbb47aa4
rgferrari <rgferrari@inf.ufsm.br>
2019-06-16 12:59:54 -0300

    Luz refactor

--- Game Jam 10/Assets/MaskPlayer.cs
+++ Game Jam 10/Assets/MaskPlayer.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class MaskPlayer : MonoBehaviour
 {
+    private int timeLeft;
+    private int timeTotal;
+    private float equation;
 
-    [Range(0.05f, 0.2f)]
+    [Range(0.05f, 1f)]
     public float flickTime;
 
-    [Range(0.02f, 0.09f)]
+    [Range(0.005f, 0.09f)]
     public float addSize;
 
     float timer = 0;
+    float smallTimer = 0;
     private bool bigger = true;
 
     public Transform target;
+
+    void Start()
+    {
+        timeTotal = GameObject.Find("Player").GetComponent<CountDown>().timeLeft;
+    }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        smallTimer = (float)timeTotal - (float)GameObject.Find("Player").GetComponent<CountDown>().timeLeft;
 
-        if(timer > flickTime) {
-            if(bigger == true){
+        transform.localScale = new Vector3(Mathf.Lerp(1f, .5f, smallTimer / timeTotal), Mathf.Lerp(1f, .5f, smallTimer / timeTotal), 1);
+
+        if (timer > flickTime)
+        {
+            if (bigger == true)
+            {
                 transform.localScale = new Vector3(transform.localScale.x + addSize, transform.localScale.y + addSize, transform.localScale.z);
-            } else {
+            }
+            else
+            {
                 transform.localScale = new Vector3(transform.localScale.x - addSize, transform.localScale.y - addSize, transform.localScale.z);
             }
 
