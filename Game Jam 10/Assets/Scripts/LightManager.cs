ac6e9c60d52dc090cd39a38ac0b0cabcdf812fdb
rgferrari <rgferrari@inf.ufsm.br>
2019-06-17 13:41:38 -0300

    Build do jogo, versão apresentada na Game Jam

---
+++ Game Jam 10/Assets/Scripts/LightManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LightManager : MonoBehaviour
+{
+    public Tilemap Darkmap;
+    public Tilemap Blurredmap; 
+    public Tilemap Walls;
+
+    public Tile Darktile;
+    public Tile Blurredtile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Darkmap.origin = Blurredmap.origin = Walls.origin;
+        Darkmap.size = Blurredmap.size = Walls.size;
+
+        foreach(Vector3Int p in Darkmap.cellBounds.allPositionsWithin){
+            Darkmap.SetTile(p, Darktile);
+        }
+
+        foreach(Vector3Int p in Blurredmap.cellBounds.allPositionsWithin){
+            Blurredmap.SetTile(p, Blurredtile);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
